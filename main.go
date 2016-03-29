f846090570971fcd1aa3881db0278de2b86a71f8
Matthew Rankin <matthew@questrail.com>
2016-03-29 14:27:41 -0500

    Show first values read for channels 1-6

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
-// Copyright (c) 2016 The usb1608fsplus developers. All rights reserved.
-// Project site: https://github.com/questrail/usb1608fsplus
+// Copyright (c) 2016 The mccdaq developers. All rights reserved.
+// Project site: https://github.com/gotmc/mccdaq
 // Use of this source code is governed by a MIT-style license that
 // can be found in the LICENSE.txt file for the project.
 
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"time"
 
 	"github.com/gotmc/libusb"
 	"github.com/gotmc/mccdaq/usb1608fsplus"
 )
+
+const millisecondDelay = 100
 
 func main() {
 	ctx, err := libusb.Init()
@@ -21,89 +25,94 @@ func main() {
 	}
 	defer ctx.Exit()
 
-	// Create the USB-1608FS-Plus DAQ device
-	daq, err := usb1608fsplus.NewViaSN(ctx, "01ACD31D")
+	// Find the first USB fevice with the VendorID and ProductID matching the MCC
+	// USB-1608FS-Plus DAQ
+	daq, err := usb1608fsplus.GetFirstDevice(ctx)
 	if err != nil {
-		log.Fatalf("Something bad getting S/N happened: %s", err)
+		log.Fatalf("Couldn't find a USB-1608FS-Plus: %s", err)
 	}
-	// If you just want to grab the first USB-1608FS-Plus that's attached, you
-	// can use:
-	// daq, err := usb1608fsplus.GetFirstDevice(ctx)
 
 	// Print some info about the device
 	log.Printf("Vendor ID = 0x%x / Product ID = 0x%x\n", daq.DeviceDescriptor.VendorID,
 		daq.DeviceDescriptor.ProductID)
 	serialNumber, err := daq.SerialNumber()
 	log.Printf("Serial number via control transfer = %s", serialNumber)
-	log.Printf("USB ConfigurationIndex = %d\n", daq.ConfigDescriptor.ConfigurationIndex)
-	log.Printf("Bulk endpoint address = 0x%x (%b)\n",
-		daq.BulkEndpoint.EndpointAddress, daq.BulkEndpoint.EndpointAddress)
-
-	// Test blinking the LED
-	blinks := 5
-	count, err := daq.BlinkLED(blinks)
-	if err != nil {
-		fmt.Errorf("Error blinking LED %s", err)
-	}
-	log.Printf("Sent %d byte of data to blink LED %d times.", count, blinks)
-
-	// Get status
-	status, err := daq.Status()
-	log.Printf("Status = %v", status)
 
 	// Read the calibration memory to setup the gain table
 	gainTable, _ := daq.BuildGainTable()
 	log.Printf("Slope = %v\n", gainTable.Slope)
 	log.Printf("Intercept = %v\n", gainTable.Intercept)
 
-	// Read one analog reading.
-	daq.AnalogInput.StopScan()
-	time.Sleep(time.Second)
-	foo, err := daq.ReadAnalogInput(1, 0)
-	if err != nil {
-		log.Fatalf("Error reading one analog input. %s", err)
-	}
-	log.Printf("Read analog input %d", foo)
-
 	/**************************
 	* Start the Analog Scan   *
 	**************************/
 
-	// Setup stuff
-	var ranges = make([]byte, 8) // Range 0 is ±10V
-	for i := 0; i < len(ranges); i++ {
-		ranges[i] = 0
-	}
-	count = 256
-	var channels byte = 0x01 // one bit for each channel
-	var frequency float64 = 20000.0
-	// options := byte(0x1 | 0x2) // immediate w/ internal pacer on
-	options := byte(0x0 | 0x2 | 0x20) // bulk w/ internal pacer on
-	numChannels := 1
+	// Create new analog input and ensure the scan is stopped and buffer cleared
+	ai := daq.NewAnalogInput()
+	ai.StopScan()
+	time.Sleep(millisecondDelay * time.Millisecond)
+	ai.ClearScanBuffer()
 
-	// Stop, clear, and configure
-	daq.AnalogInput.StopScan()
-	time.Sleep(time.Second)
-	daq.ClearScanBuffer()
-	daq.AnalogInput.SetScanRanges(ranges)
-	time.Sleep(2 * time.Second)
-	blah, err := daq.AnalogInput.ScanRanges()
-	log.Printf("Ranges = %v\n", blah)
-
-	// Start the scan
-	daq.AnalogInput.StartScan(count)
-	time.Sleep(1 * time.Second)
-	data, err := daq.AnalogInput.ReadScan(count, numChannels, options)
-	for i := 0; i < 8; i += 2 {
-		log.Printf("data[%d:%d] = %d %d\n", i, i+1, data[i+1], data[i])
+	// Setup the analog input scan
+	configData, err := ioutil.ReadFile("./analog_config.json")
+	if err != nil {
+		log.Fatalf("Error reading the USB-1608FS-Plus JSON config file")
 	}
-	for i := count - 8; i < count; i += 2 {
-		log.Printf("data[%d:%d] = %d %d\n", i, i+1, data[i+1], data[i])
+	dec := json.NewDecoder(bytes.NewReader(configData))
+	var configJSON = struct {
+		*usb1608fsplus.AnalogInput `json:"analog_input"`
+	}{
+		ai,
 	}
-	log.Printf("data is %d bytes\n", len(data))
-	daq.AnalogInput.StopScan()
-	time.Sleep(1 * time.Second)
+	if err := dec.Decode(&configJSON); err != nil {
+		log.Fatalf("parse USB-1608FS-Plus: %v", err)
+	}
+	ai.SetScanRanges()
+	log.Printf("Frequency = %f Hz", ai.Frequency)
 
+	// Read the scan ranges
+	time.Sleep(millisecondDelay * time.Millisecond)
+	scanRanges, err := ai.ScanRanges()
+	log.Printf("Ranges = %v\n", scanRanges)
+
+	// Read the totalScans using splitScansIn number of scans
+	const (
+		scansPerBuffer = 256
+		totalBuffers   = 10
+	)
+	expectedDuration := (scansPerBuffer * totalBuffers) / ai.Frequency
+	ai.StartScan(0)
+	start := time.Now()
+	totalBytesRead := 0
+	for j := 0; j < totalBuffers; j++ {
+		// time.Sleep(millisecondDelay * time.Millisecond)
+		data, err := ai.ReadScan(scansPerBuffer)
+		totalBytesRead += len(data)
+		if err != nil {
+			// Stop the analog scan and close the DAQ
+			ai.StopScan()
+			time.Sleep(millisecondDelay * time.Millisecond)
+			daq.Close()
+			log.Fatalf("Error reading scan: %s", err)
+		}
+		// Show the first value measured for each channel
+		wordsToShow := 6
+		for i := 0; i < wordsToShow; i++ {
+			raw, err := usb1608fsplus.Volts(data[i*2:i*2+2], ai.Channels[i].Range)
+			if err != nil {
+				log.Printf("%s = 0x%02x%02x (Error: %s)\n", ai.Channels[i].Description, data[i*2+1], data[i*2], err)
+			} else {
+				log.Printf("%s = %.5f Vraw\n", ai.Channels[i].Description, raw)
+			}
+		}
+	}
+	elapsed := time.Since(start)
+	log.Printf("Reading %d bytes took %.2f s", totalBytesRead, elapsed.Seconds())
+	log.Printf("Anticipated reading %d bytes to take %.2f s",
+		scansPerBuffer*totalBuffers*ai.NumEnabledChannels()*2, expectedDuration)
+	// Stop the analog scan and close the DAQ
+	time.Sleep(millisecondDelay * time.Millisecond)
+	ai.StopScan()
+	time.Sleep(millisecondDelay * time.Millisecond)
 	daq.Close()
-
 }
